8878c4cf83bc1a283c9c041fa26868cd14cb2294
Jose Augusto Cenci Castilho <ze.ccenci@gmail.com>
2024-10-09 16:22:51 +0100

    adding structural functions to read and write the json todolists

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:flutter/material.dart';
@@ -17,6 +18,9 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  //list of todos
+  List _todoList = [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,7 +30,7 @@ class _HomeState extends State<Home> {
       ),
       body: Container(
         color: Colors.white,
-        child: Center(
+        child: const Center(
           child: Text(
             'Conteúdo',
             style: TextStyle(
@@ -38,10 +42,32 @@ class _HomeState extends State<Home> {
       ),
     );
   }
-}
 
-Future<File> _getFile() async {
-  final directory = await getApplicationDocumentsDirectory();
-  //local do arquivo vou salvar
-  return File('${directory.path}/data.json');
+  Future<File> _getFile() async {
+    final directory = await getApplicationDocumentsDirectory();
+    //local do arquivo vou salvar
+    return File('${directory.path}/data.json');
+  }
+
+  Future<File> _saveData() async {
+    //pegamos o arquivo e convertemos para json
+    String data = json.encode(_todoList);
+
+    //esperar para obter o arquivo
+    final file = await _getFile();
+
+    //escrever no arquivo
+    return file.writeAsString(data);
+  }
+
+  Future<String> _readData() async {
+    try {
+      final file = await _getFile();
+
+      //como ja estao salvos como string, basta ler como str
+      return file.readAsString();
+    } catch (e) {
+      throw Exception('Erro ao ler os dados ' + e.toString());
+    }
+  }
 }
